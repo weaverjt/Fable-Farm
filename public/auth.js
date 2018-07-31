53887473fd72ab7c84e68e12e4ee90563500932d
Weaverjt <weaver.jacob.t@gmail.com>
2018-07-31 00:35:48 -0500

    The create new character/story thing is getting there

--- public/auth.js
+++ public/auth.js
@@ -14,51 +14,84 @@ firebase.initializeApp(config);
 const database = firebase.database()
 const auth = firebase.auth()
 
-// Access the values from the html forms
-const storyForm = document.getElementById('login')
-
-const loginEmail = document.getElementById('login-email')
-
-const loginPassword = document.getElementById('login-password')
-
-
-/* const loginButton = document.getElementById('login-button') */
-
-const characterForm = document.getElementById('sign-up')
-
-const signUpEmail = document.getElementById('sign-up-email')
-
-const signUpPassword= document.getElementById('sign-up-password')
-
-/* const signUpButton = document.getElementById('sign-up-button') */
-
-const logoutButton = document.getElementById('logout-button')
-
 const status = document.getElementById('status')
 const errors = document.getElementById('errors')
 
-const addItemForm = document.getElementById('add-item-form')
-const toDoList = document.getElementById('to-do-list')
+const storyForm = document.getElementById('storyForm')
+const myStoryList = document.getElementById('myStoryList')
 
-// Log in
-storyForm.addEventListener('click', e => {
-  console.log('log in was clickd');
-  e.preventDefault()
+const characterForm = document.getElementById('characterForm')
+const myCharacterList = document.getElementById('myCharacterList')
+
+
+
+const logoutButton = document.getElementById('logout-button')
+
+// create new story
+storyForm.addEventListener('submit', e => {
+    console.log('Story was added to your list!');
+    e.preventDefault()
+    
+  })
   
-})
 
-// Sign up
-characterForm.addEventListener('click', e => {
-  console.log('sign up was clicked');
-  e.preventDefault() 
-})
+// create new character
+characterForm.addEventListener('submit', e => {
+    console.log('Character was added to your list!');
+    e.preventDefault() 
+  })
+
+// Log out
+logoutButton.addEventListener('click', () => {
+    console.log("Log out was clicked")
+    auth.signOut()
+  })
+
 
 // UID is global so it can be accessed anywhere
 var uid = ""
 
 // Firebase monitors the Auth state in real time. Use this if/else statement to do things based on that state.
 firebase.auth().onAuthStateChanged(firebaseUser => {
-  if (toggleOn){
+    if (firebaseUser){
+  
+      //Everything in this if is for a logged in user
+      console.log(firebaseUser)
+      logoutButton.classList.remove('hide')
+     /*  status.innerHTML = 'Status: <span class="status-green">logged in</span>' */
+      uid = firebaseUser.uid
+      localStorage.setItem("uid", uid) 
+     
+  
+      // Use the UID to get authorized data
+   /*    database.ref('/to-do-list').orderByChild('uid').equalTo(uid).on("value", (snapshot) => {
+        document.getElementById('to-do-list-items').innerHTML = ""
+        snapshot.forEach(data => {
+          var p = document.createElement('p')
+          p.textContent = data.val().item
+          var deleteButton = document.createElement('button')
+          deleteButton.textContent = 'x'
+          deleteButton.classList.add('delete-button')
+          deleteButton.setAttribute('data', data.key)
+          p.appendChild(deleteButton)
+          document.getElementById('to-do-list-items').appendChild(p)
+        })
+      }) */
+  
+    } else {
+  
+      //Everything in this "else" is when no one is logged in
+      console.log('not logged in')
+      logoutButton.classList.add('hide')
+      location.setItem('index.html')
+   /*    status.innerHTML = 'Status: <span class="status-red">not logged in</span>' */
+    }
+  })
+
+
+/* 
+firebase.auth().onToggle(firebaseUser => {
+  if (toggle == true){
 
     //Everything in this if is for a logged in user
     console.log("Stories")
@@ -74,7 +107,7 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
     storyForm.classList.add('hide')
     characterForm.classList.remove('hide')
   }
-})
+}) */
 
 var displayError = (message) => {
   console.log('this happened');
