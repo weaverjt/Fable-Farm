a53c4a33c90680ab546c304df5e925c975c8b56b
Edison <edisonyang527@gmail.com>
2018-08-02 14:08:49 -0500

    fragment page created

--- public/js/createFrag.js
+++ public/js/createFrag.js
@@ -1,13 +1,25 @@
 
 $(document).on("click","#submitFrag",function(){
-
+    console.log("clicked");
     var userId=$.cookie('userId')
+     console.log(userId);
+
+   var urlfilter=window.location.hash;
+
+   console.log("urlfilter",urlfilter);
+
+   var storyId = urlfilter.slice(1); 
+   
+   console.log(storyId);
+
+     
+
 
     if(userId) {
         console.log("submit yeaaahhh");
         var data={
             UserId: userId,
-            StoryId: $("#storyId").val().trim(),
+            StoryId: storyId,
             fragmentText:$("#fragText").val().trim(),
             status: $("#status option:selected").val(),
     
@@ -28,7 +40,7 @@ $(document).on("click","#submitFrag",function(){
     else
     {
         alert("you are not logged in , please log-in first");
-        window.location.href("/login");
+        location.replace("/");
     }
 
 
--- public/js/fable.js
+++ public/js/fable.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
             
             $("#storyList> tbody").append(`<tr class="fables"><td class="col-md-2" style="font-size: 70%">` + data[i].title + '</td><td class="col-md-2"  style="font-size: 70%">' +
             data[i].settings + `</td><td class="col-md-4" style="font-size: 70%">` + data[i].storyText + `</td><td class="col-md-1"  style="font-size: 70%">` + data[i].Category.catName 
-            + `</td><td class="col-md-1">` + `<button class="btn createNewFrag" value="$${data[i].id}"  style="font-size: 70%">Add To Story</button></td><td class="col-md-2"  style="font-size: 70%">`
+            + `</td><td class="col-md-1">` + `<button class="btn createNewFrag" value="${data[i].id}"  style="font-size: 70%">Add To Story</button></td><td class="col-md-2"  style="font-size: 70%">`
             + `<button id="story${data[i].id}-Btn" class="btn viewStoryBtn" value="${data[i].id}">View Full Story</button>` + "</td></tr>");
             
 
@@ -20,14 +20,14 @@ $(document).ready(function(){
 $(document).on("click",".viewStoryBtn",function(){
     event.preventDefault(); 
     var storyID = $(this).val();
-    location.replace(/stories/ + storyID);
+    location.replace("/stories/storyid#" + storyID);
 }
 )
 
 $(document).on("click",".createNewFrag",function(){
     event.preventDefault(); 
     var storyID = $(this).val();
-    location.replace(/createFrag/ + storyID);
+    location.replace("/createFrag/storyId#" + storyID);
 }
 )
 
