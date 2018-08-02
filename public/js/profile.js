3d0cac458c2a96b7ae031268ba90abb0ac7ca3b3
KMonahan <kevin.monahan@hotmail.com>
2018-08-02 11:57:13 -0500

    pulling changes

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,39 +1,39 @@
 let id;
 let name;
+let imageUrl;
 
 
-$(document).ready(function(){
+$(document).ready(function () {
     console.log("Hello Profile");
-     
-    var userUId=$.cookie('uid');
-    var userId=$.cookie('userId');
-    console.log("userId cookie is: ",userId);
 
-    if (userUId)
-    {   
+    var userUId = $.cookie('uid');
+    var userId = $.cookie('userId');
+    console.log("userId cookie is: ", userId);
+
+    if (userUId) {
         console.log(userUId);
-        $.get("/api/uid/"+userUId).then(
-            function(data){
-        console.log(" id for this user is : ", data.id);
-        id=data.id; 
-        name=data.userName;
+        $.get("/api/uid/" + userUId).then(
+            function (data) {
+                console.log(" id for this user is : ", data.id);
+                id = data.id;
+                name = data.userName;
+                imageUrl = data.imageURL;
 
-        RetrieveUserInfo(id);
-        })
+                RetrieveUserInfo(id);
+                $("#userImg").attr("src", imageUrl);
+            })
 
     }
-    else if (userId)
-    {
-        console.log("userId is : ",userId)
+    else if (userId) {
+        console.log("userId is : ", userId)
         RetrieveUserInfo(userId);
-        
+
     }
-    else
-    {
+    else {
         alert("no user sign-in, no profile to display!");
         console.log("error sign-in");
     }
-    
+
 
 })
 
@@ -42,27 +42,26 @@ function RetrieveUserInfo(id) {
     $("#storyContainer").empty();
     $("#fragContainer").empty();
 
-    console.log("welcome : ",name);
-   
-    console.log("user id is : ",id);   
+    console.log("welcome : ", name);
 
-    $.get("/api/users/"+id,function(data){
+    console.log("user id is : ", id);
+
+    $.get("/api/users/" + id, function (data) {
         console.log(data);
 
-    for (var c in data.Characters)
-    {
-        $("#charContainer").append(`<p>we found :  ${data.Characters[c].charName} as a character</p>`)
-    }
+        for (var c in data.Characters) {
+            $("#charContainer").append(`<p>we found :  ${data.Characters[c].charName} as a character</p>`)
+        }
 
-    for (var s in data.Stories)
-    {
-        $("#storyContainer").append(`<p>we found that you are the ADMIN/CREATOR :  ${data.Stories[s].title}</p>`)
-    }
+        for (var s in data.Stories) {
+            $("#storyContainer").append(`<p>we found that you are the ADMIN/CREATOR :  ${data.Stories[s].title}</p>`)
+        }
 
-    for (var f in data.Fragments)
-    {
-        $("#fragContainer").append(`<p>we found :  ${data.Fragments[f].fragmentText} as a fragment of story id : ${data.Fragments[f].StoryId}<p>`)
-    }
+        for (var f in data.Fragments) {
+            $("#fragContainer").append(`<p>we found :  ${data.Fragments[f].fragmentText} as a fragment of story id : ${data.Fragments[f].StoryId}<p>`)
+        }
 
     })
+
+
 }
